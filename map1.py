2f961d5bc7f895ecba9d6f68922c3de9a029079a
Sari Kanamarlapudi <skanamarla@gmail.com>
2020-05-17 16:45:10 -0700

    Adding points from files

--- map1.py
+++ map1.py
@@ -1,11 +1,16 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
 map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-#normally cooridantes will be in textfile
-for coordinates in [[38, -99],[39, -97]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Henlo am marker", icon=folium.Icon(color='blue')))
+for lt, ln in zip(lat,lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Henlo am marker", icon=folium.Icon(color='blue')))
 
 map.add_child(fg)
 
